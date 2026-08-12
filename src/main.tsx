199c2a1498b7e2371919120f58f86c7e73ef09c6
Diego <diego@docalivre.com>
2026-08-12 16:57:51 -0300

    chore: forcar cache novo apos ajustes de rotas e endereco

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
  * Força novo bundle (setas da galeria). Depois do primeiro load limpo,
  * updates de deploy só no F5 (ver onNeedRefresh).
  */
-const BUILD_ID = 'motorista-cnh-anexo-v1'
+const BUILD_ID = 'rotas-passagem-km-v2'
 
 async function forceFreshOnce(): Promise<boolean> {
   const key = `doca-build:${BUILD_ID}`
